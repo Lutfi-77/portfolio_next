e864dfbfd445da66d3e4f2ae323e7b9086abe276
Lutfi-77 <xenonoffic@gmail.com>
2026-09-01 19:24:00 +0700

    add animation to text

--- app/page.jsx
+++ app/page.jsx
@@ -1,52 +1,11 @@
 import Image from 'next/image';
-import { assets } from '@/utils/assets';
 import { projects } from '@/utils/projects';
+import Hero from '@/components/hero';
 
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <section className="hero pb-28">
-        <div className="grid grid-cols-1 md:grid-cols-2 pt-5 md:mt-36">
-          <div className="profile-text m-auto">
-            <h3>
-              <span className="bg-green-600 w-3 h-3 mr-2 inline-block rounded-full"></span>
-              Available for hire
-            </h3>
-            <h1 className="text-4xl">
-              Hi, I'm Mohammad Dawan Lutfi a
-              <span className="text-gray-400"> Web Developer</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus, tempora quasi aliquid ex facere, corrupti suscipit
-              animi at adipisci ut sit eaque voluptatibus similique. Impedit,
-              eius. Suscipit placeat laudantium consequatur.
-            </p>
-            <div className="social flex gap-3 pt-5">
-              <div className="py-1 px-3 rounded-full bg-card shadow-card hover:shadow-hover duration-300 transition-all ease-in-out">
-                Instagram
-              </div>
-              <div className="py-1 px-3 rounded-full bg-card shadow-card hover:shadow-hover duration-300 transition-all ease-in-out">
-                Instagram
-              </div>
-              <div className="py-1 px-3 rounded-full bg-card shadow-card hover:shadow-hover duration-300 transition-all ease-in-out">
-                Instagram
-              </div>
-            </div>
-          </div>
-          <div className="image mt-auto">
-            <div className="mx-auto w-1/2 rounded-2xl p-[2px] bg-gradient-to-bl from-green-600 via-transparent to-transparent overflow-hidden">
-              <div className="rounded-2xl overflow-hidden bg-white dark:bg-background">
-                <Image
-                  src={assets.profile}
-                  alt="profile"
-                  className="rounded-2xl w-full h-auto object-cover block"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Hero />
 
       <section className="my-works">
         <h1 className="text-3xl text-center after:content-[''] after:w-1/4 after:h-1 after:block after:m-auto after:rounded-full after:bg-foreground after:mt-2 pb-3">
---
+++ components/hero.jsx
@@ -0,0 +1,81 @@
+'use client';
+
+import React from 'react';
+import { assets } from '@/utils/assets';
+import { motion } from 'motion/react';
+import Image from 'next/image';
+
+const Hero = () => {
+  const container = {
+    start: { opacity: 0 },
+    to: { opacity: 1, transition: { staggerChildren: 0.18 } },
+  };
+
+  const items = {
+    start: { opacity: 0, x: -50 },
+    to: { opacity: 1, x: 0 },
+  };
+
+  return (
+    <>
+      <section className="hero pb-28">
+        <div className="grid grid-cols-1 md:grid-cols-2 pt-5 md:mt-36">
+          <motion.div
+            variants={container}
+            initial="start"
+            animate="to"
+            className="profile-text m-auto"
+          >
+            <motion.h3 variants={items}>
+              <span className="bg-green-600 w-3 h-3 mr-2 inline-block rounded-full"></span>
+              Available for hire
+            </motion.h3>
+            <motion.h1 variants={items} className="text-4xl">
+              Hi, I'm Mohammad Dawan Lutfi a
+              <span className="text-gray-400"> Web Developer</span>
+            </motion.h1>
+            <motion.p variants={items}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, tempora quasi aliquid ex facere, corrupti suscipit
+              animi at adipisci ut sit eaque voluptatibus similique. Impedit,
+              eius. Suscipit placeat laudantium consequatur.
+            </motion.p>
+            <div className="social flex gap-3 pt-5">
+              <motion.div
+                variants={items}
+                className="py-1 px-3 rounded-full bg-card shadow-card hover:shadow-hover duration-300 transition-shadow ease-in-out"
+              >
+                Instagram
+              </motion.div>
+              <motion.div
+                variants={items}
+                className="py-1 px-3 rounded-full bg-card shadow-card hover:shadow-hover duration-300 transition-shadow ease-in-out"
+              >
+                Instagram
+              </motion.div>
+              <motion.div
+                variants={items}
+                className="py-1 px-3 rounded-full bg-card shadow-card hover:shadow-hover duration-300 transition-shadow ease-in-out"
+              >
+                Instagram
+              </motion.div>
+            </div>
+          </motion.div>
+          <div className="image mt-auto">
+            <div className="ml-auto w-1/2 rounded-2xl p-[2px] bg-gradient-to-bl from-green-600 via-transparent to-transparent overflow-hidden">
+              <div className="rounded-2xl overflow-hidden bg-white dark:bg-background">
+                <Image
+                  src={assets.profile}
+                  alt="profile"
+                  className="rounded-2xl w-full h-auto object-cover block"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
