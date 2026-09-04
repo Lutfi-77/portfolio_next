007225000e75590bcd6919d2327ab44ac72c70f0
Lutfi-77 <xenonoffic@gmail.com>
2026-09-04 17:04:17 +0700

    update nextjs

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,10 @@ import { Roboto, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import SideNav from '@/components/side-nav';
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 const fontSans = Roboto({
   variable: '--font-sans',
   subsets: ['latin'],
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,10 @@ import AboutMe from '@/components/about-me';
 import Hero from '@/components/hero';
 import MyWorks from '@/components/my-works';
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 export default function Home() {
   return (
     <div className="container mx-auto">
